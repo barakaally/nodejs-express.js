486b45d74dc42340ded1b2e705cc13cc0793d8cc
barakaally <baraka.ally.co.tz@gmail.com>
2020-02-01 12:53:13 +0300

    expose public folder via static middleware

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const keys = require("./keys");
-const Router = require("./routes/router");
+const router = require("./routes/router");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
-
 app.use(bodyParser.json(), cors());
 
-Router.addRoutes(app);
+app.use(express.static('public'));
+
+router.addRoutes(app);
 
 app.listen(keys.PORT, () => {
     console.log(`server started on port  ${keys.PORT}`);
